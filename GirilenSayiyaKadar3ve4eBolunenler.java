0a3347648d4b8ba4eed196500f141a7130875625
Ozan Recber <121625150+OzanRecber@users.noreply.github.com>
2023-01-04 16:31:52 +0300

    Add files via upload

--- GirilenSayiyaKadar3ve4eBolunenler.java
+++ GirilenSayiyaKadar3ve4eBolunenler.java
@@ -5,16 +5,23 @@ import java.util.Scanner;
 public class GirilenSayiyaKadar3ve4eBolunenler {
 
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("Bir sayı değeri giriniz: ");
-		int n = input.nextInt();
-		int i = 0;
-		while(i<=n) {
-			if(i%3==0 && i%4==0) {
-				System.out.print(i + " ");
-			}
-			i++;
-		}
+		int num = input.nextInt(); // verilen sayı
+	    int sum = 0; // toplam değişkeni
+	    int count = 0; // sayı sayacı
+
+	    for (int i = 1; i <= num; i++) {
+	      if (i % 3 == 0 && i % 4 == 0) { // i 3 veya 4'e tam bölünüyorsa
+	    	  System.out.print(i+" ");
+	        sum += i; // toplama ekle
+	        count++; // sayı sayacını arttır
+	      }
+	    }
+
+	    double average = (double) sum / count; // ortalama hesapla
+	    System.out.println("\nOrtalama: " + average);
 		
 	}
 
