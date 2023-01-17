40b1ca5ae08be7a73fb540f7f65c7e25ca7ab958
Ozan Recber <121625150+OzanRecber@users.noreply.github.com>
2023-01-17 12:17:21 +0300

    Add files via upload

---
+++ BasamakSayilariToplami.java
@@ -0,0 +1,22 @@
+package donguler;
+
+import java.util.Scanner;
+
+public class BasamakSayilariToplami {
+
+	public static void main(String[] args) {
+		// Bir sayının basamak sayılarının toplamını hesaplayan program yazınız.
+		// Örnek : 1643 = 1 + 6 + 4 + 3 = 14
+		Scanner input = new Scanner(System.in);
+		System.out.print("Bir sayı giriniz: ");
+		int sayi = input.nextInt();
+		int toplam = 0;
+		while(sayi>0) {
+			toplam += sayi%10;
+			sayi /= 10;
+		}
+		System.out.println("Sayının basamak değerleri toplamı = " + toplam);
+		 
+	}
+
+}
