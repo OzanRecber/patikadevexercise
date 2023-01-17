fb21592f5886f2f3491547b07ae7dc4a8b9e2b52
Ozan Recber <121625150+OzanRecber@users.noreply.github.com>
2023-01-17 12:15:12 +0300

    Add files via upload

--- UsluSayi.java
+++ UsluSayi.java
@@ -1,26 +1,28 @@
-package metotlar;
+package donguler;
 
 import java.util.Scanner;
 
 public class UsluSayi {
-	public static double exponentiate(double base, int exponent) {
-        if (exponent == 0) {
-            return 1;
-        }
-        if(exponent < 0) {
-            exponent = -exponent;
-            base = 1/base;
-        }
-        return base * exponentiate(base, exponent - 1);
-    }
 
-    public static void main(String[] args) {
-    	Scanner input = new Scanner(System.in);
-    	System.out.print("Taban değerini giriniz: ");
-        double base =input.nextDouble();
-        System.out.print("Üs değerini giriniz: ");
-        int exponent = input.nextInt();
-        double result = exponentiate(base, exponent);
-        System.out.println(base + "^" + exponent + " = " + result);
-    }
+	public static void main(String[] args) {
+		// Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan programı "For Döngüsü" kullanarak yapınız.
+		Scanner input = new Scanner(System.in);
+		System.out.print("Taban sayısını giriniz: ");
+		double taban = input.nextDouble();
+		System.out.print("Üs sayısını giriniz: ");
+		int us = input.nextInt();
+		double sonuc = 1;
+		if(us < 0) {
+			us = -us;
+			taban = 1/taban;
+			for(int i = 1; i <= us; i++) {
+				sonuc *= taban;
+			}
+		} else {
+			for(int i = 1; i <= us; i++) {
+				sonuc *= taban;	
+			}
+		}
+		System.out.println("Sonuç: " + sonuc);
+	}
 }
