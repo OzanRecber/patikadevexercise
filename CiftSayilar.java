962b8188c1f984cd4854ff2f298fc5b5d0cddced
Ozan Recber <121625150+OzanRecber@users.noreply.github.com>
2023-01-17 11:50:22 +0300

    Add files via upload

---
+++ CiftSayilar.java
@@ -0,0 +1,20 @@
+package donguler;
+
+import java.util.Scanner;
+
+public class CiftSayilar {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Sayı değeri giriniz: ");
+		int n = input.nextInt();
+		for(int i = 1; i<=n; i++) {
+			if(i%2==0) {
+				System.out.print(i + " ");
+			}
+			
+		}
+		
+	}
+	
+}
